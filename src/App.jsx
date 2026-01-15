a670aba8b5acab43116829ecd482edbbb12fb1ca
Siddhant-Kumar-04 <Siddhant-Kumar-04@users.noreply.github.com>
2026-01-15 13:47:01 +0530

    Nested Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import  CustomComponent from './Componet.jsx';
 function App() {
   return (
-    <h1 className="font-semibold text-3xl">Hello World</h1>
+    <div>
+      <h1 className="font-semibold text-3xl">Hello World</h1>
+      <CustomComponent/>
+    </div>
   )
 }
 export default App
---
+++ src/Componet.jsx
@@ -0,0 +1,13 @@
+function CustomComponent() { 
+    return (
+        <div><OneMore /></div>
+    )
+}
+
+function OneMore() {
+    return (
+        <h1>Hello OneMore</h1>
+    )
+}
+
+export default CustomComponent;
